e5e14532cc2c7bd36e26a5e5def4f6214556189c
scisci <dpriley@ucla.edu>
2015-10-16 12:45:48 -0400

    Prefix error

--- build-lib.sh
+++ build-lib.sh
@@ -1,1 +1,1 @@
-gomobile bind -target=ios -o lib/lib.framework github.com/scisci/gomobile-static-test/lib
+gomobile bind -target=ios -prefix Lib -o lib/lib.framework github.com/scisci/gomobile-static-test/lib
